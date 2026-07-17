4e4a49d71f6e400b5a7b9b9710de8221267cdb08
sebha01 <sebastianha175@gmail.com>
2026-07-17 14:05:52 +0100

    Froze bat rigid body z rotation, updated collision to not trigger game over on bounds collision overlap, added bounds colliders to keep the bat within the map

--- Assets/Scripts/BatScript.cs
+++ Assets/Scripts/BatScript.cs
@@ -9,19 +9,31 @@ public class BatScript : MonoBehaviour
     //public
     public Rigidbody2D myRigidBody;
     public float jumpStrength = 2.0f;
+    public LogicScript logic;
+    public bool batIsAlive = true;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        batIsAlive = true;
         JumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (JumpAction.IsPressed())
+        if (JumpAction.IsPressed() && batIsAlive)
         {      
             myRigidBody.linearVelocity = Vector2.up * jumpStrength; 
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Bounds"))
+        {
+            logic.GameOver();
+            batIsAlive = false;
         }
     }
 }
--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -6,6 +6,7 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void AddScore(int scoreToAdd)
@@ -17,6 +18,11 @@ public class LogicScript : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 
 }
