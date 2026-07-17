3734933f32ad7d28eafc939a5981cfb7c7da1ad1
sebha01 <sebastianha175@gmail.com>
2026-07-17 12:28:13 +0100

    Created pillar objects and got initial script working, adjusted pillar sprites

---
+++ Assets/Scripts/PillarMoveScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PillarMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+    }
+}
